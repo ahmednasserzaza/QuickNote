be925d3f9d2f972ec2d47e64d642b6ba6406fae7
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 11:08:22 +0300

    implement NoteEntity

--- app/src/main/java/com/fighter/quicknote/data/local/NoteEntity.kt
+++ app/src/main/java/com/fighter/quicknote/data/local/NoteEntity.kt
@@ -1,3 +1,13 @@
 package com.fighter.quicknote.data.local
 
-data class NoteEntity()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "NOTE_TABLE")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val title: String,
+    val content: String,
+    val date: Date,
+)
