55b62a057d3ffbe4bda52f1f0f8c40b38e53ebb8
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 11:10:26 +0300

    implement NoteDao

--- app/src/main/java/com/fighter/quicknote/data/local/NoteDao.kt
+++ app/src/main/java/com/fighter/quicknote/data/local/NoteDao.kt
@@ -1,4 +1,25 @@
 package com.fighter.quicknote.data.local
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note: NoteEntity)
+
+    @Delete
+    suspend fun deleteNote(note: NoteEntity)
+
+    @Update
+    suspend fun updateNote(note: NoteEntity)
+
+    @Query("SELECT * FROM NOTE_TABLE ORDER BY id DESC")
+    fun getAllNotes(): Flow<List<NoteEntity>>
+
+    @Query("SELECT * FROM NOTE_TABLE WHERE content LIKE :searchTerm ORDER BY id DESC")
+    suspend fun getFilteredNotes(searchTerm: String): List<NoteEntity>
 }
