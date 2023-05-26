9cc64686470d54fdeb31530d7b741fc469ed82b1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 11:40:19 +0300

    implement NoteRepositoryImp

--- app/src/main/java/com/fighter/quicknote/data/repositories/NoteRepository.kt
+++ app/src/main/java/com/fighter/quicknote/data/repositories/NoteRepository.kt
@@ -1,4 +1,12 @@
 package com.fighter.quicknote.data.repositories
 
+import com.fighter.quicknote.data.local.NoteEntity
+import kotlinx.coroutines.flow.Flow
+
 interface NoteRepository {
+    suspend fun insertNewNote(note: NoteEntity)
+
+    fun getAllNotes(): Flow<List<NoteEntity>>
+
+    suspend fun getFilteredNotes(searchTerm: String): List<NoteEntity>
 }
--- app/src/main/java/com/fighter/quicknote/data/repositories/NoteRepositoryImp.kt
+++ app/src/main/java/com/fighter/quicknote/data/repositories/NoteRepositoryImp.kt
@@ -1,4 +1,15 @@
 package com.fighter.quicknote.data.repositories
 
-class NoteRepositoryImp  {
+import com.fighter.quicknote.data.local.NoteDao
+import com.fighter.quicknote.data.local.NoteEntity
+import javax.inject.Inject
+
+class NoteRepositoryImp @Inject constructor(private val dao: NoteDao) : NoteRepository {
+
+    override suspend fun insertNewNote(note: NoteEntity) = dao.insertNote(note)
+
+    override fun getAllNotes() = dao.getAllNotes()
+
+    override suspend fun getFilteredNotes(searchTerm: String) =
+        dao.getFilteredNotes("%$searchTerm%")
 }
