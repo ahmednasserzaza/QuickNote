f4d11f8c257fb73fd11a8d03af5df0e26d6f3640
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 14:28:12 +0300

    implement NoteViewModel

--- app/src/main/java/com/fighter/quicknote/ui/notes/NoteViewModel.kt
+++ app/src/main/java/com/fighter/quicknote/ui/notes/NoteViewModel.kt
@@ -1,9 +1,52 @@
 package com.fighter.quicknote.ui.notes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.fighter.quicknote.data.local.NoteEntity
 import com.fighter.quicknote.data.repositories.NoteRepository
 import com.fighter.quicknote.ui.base.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
+import java.util.Date
 import javax.inject.Inject
 
-class NoteViewModel @Inject constructor(noteRepository: NoteRepository) : BaseViewModel() {
+class NoteViewModel @Inject constructor(private val noteRepository: NoteRepository) :
+    BaseViewModel() {
+
+    val newNoteTitle = MutableLiveData<String>()
+    val newNoteContent = MutableLiveData<String>()
+
+    val searchTerm = MutableStateFlow("")
+
+    private val _notes = MutableLiveData<List<NoteEntity>>()
+    val notes: LiveData<List<NoteEntity>>
+        get() = _notes
+
+    init {
+        getAllNotes()
+    }
+
+
+    private fun getAllNotes() {
+        viewModelScope.launch {
+            noteRepository.getAllNotes().collect {
+                _notes.postValue(it)
+            }
+        }
+    }
+
+    fun addNote() {
+        viewModelScope.launch {
+            newNoteTitle.value?.let { title ->
+                newNoteContent.value?.let { content ->
+                    noteRepository.insertNewNote(NoteEntity(0, title, content, Date()))
+                    newNoteTitle.value = ""
+                }
+            }
+        }
+    }
+
 
 }
