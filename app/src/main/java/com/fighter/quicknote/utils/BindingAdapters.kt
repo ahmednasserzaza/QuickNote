ff070717fa74729512465eb7127f1eeb3e6655c9
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 14:27:51 +0300

    add setRecyclerItems binding adapter function

--- app/src/main/java/com/fighter/quicknote/utils/BindingAdapters.kt
+++ app/src/main/java/com/fighter/quicknote/utils/BindingAdapters.kt
@@ -1,2 +1,15 @@
 package com.fighter.quicknote.utils
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.fighter.quicknote.ui.base.BaseAdapter
+
+@BindingAdapter(value = ["app:items"])
+fun<T> setRecyclerItems(view: RecyclerView, items:List<T>?){
+    if (items != null){
+        (view.adapter as BaseAdapter<T>?)?.setItems(items)
+    }
+    else{
+        (view.adapter as BaseAdapter<T>?)?.setItems(emptyList())
+    }
+}
