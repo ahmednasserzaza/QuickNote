488a6d6b98e4765591269e6c914791406ada4ac4
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 12:20:12 +0300

    implement NoteAdapter

--- app/src/main/java/com/fighter/quicknote/ui/notes/NoteAdapter.kt
+++ app/src/main/java/com/fighter/quicknote/ui/notes/NoteAdapter.kt
@@ -1,4 +1,18 @@
 package com.fighter.quicknote.ui.notes
 
-class NoteAdapter {
+import com.fighter.quicknote.R
+import com.fighter.quicknote.data.local.NoteEntity
+import com.fighter.quicknote.ui.base.BaseAdapter
+import com.fighter.quicknote.ui.base.BaseInteractionListener
+
+
+class NoteAdapter(
+    notes: List<NoteEntity>,
+    listener: NoteInteractionListener
+) : BaseAdapter<NoteEntity>(notes, listener) {
+    override val layoutID: Int = R.layout.item_note
+}
+
+interface NoteInteractionListener : BaseInteractionListener {
+    fun onClickNote(note: NoteEntity)
 }
