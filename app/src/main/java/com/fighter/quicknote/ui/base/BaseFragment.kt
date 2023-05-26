b3b8a0f426a7c8487691a2e21acd85d2502d33d6
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 10:46:21 +0300

    implement BaseFragment

--- app/src/main/java/com/fighter/quicknote/ui/base/BaseFragment.kt
+++ app/src/main/java/com/fighter/quicknote/ui/base/BaseFragment.kt
@@ -1,4 +1,34 @@
 package com.fighter.quicknote.ui.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.fighter.quicknote.BR
+
+abstract class BaseFragment<VDB : ViewDataBinding> : Fragment() {
+    abstract val layoutIdFragment: Int
+    abstract val viewModel: ViewModel
+
+    private lateinit var _binding: VDB
+    protected val binding: VDB
+        get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutIdFragment, container, false)
+        _binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, viewModel)
+            return root
+        }
+    }
+
 }
