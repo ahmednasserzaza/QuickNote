3b3ade23850d708bf6b0f2dc495b15ddeef698ab
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-26 11:12:28 +0300

    implement Converters.kt

--- app/src/main/java/com/fighter/quicknote/data/local/Convertes.kt
+++ app/src/main/java/com/fighter/quicknote/data/local/Convertes.kt
@@ -1,2 +1,14 @@
 package com.fighter.quicknote.data.local
 
+import androidx.room.TypeConverter
+import java.util.Date
+
+@TypeConverter
+fun dateToLong(date: Date):Long{
+    return date.time
+}
+
+@TypeConverter
+fun longToDate(long:Long): Date {
+    return Date(long)
+}
